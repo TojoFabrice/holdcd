9e9e3b073c4800b3423827b1b6ddd4779dc1fac2
Fabrice <fabrice@wavetech.dev>
2024-08-14 12:50:00 +0300

    maj

--- src/components/aboutSection.tsx
+++ src/components/aboutSection.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function AboutSection() {
   return (
-    <section id='about' className='w-4/5 bg-[#F9F8F8] p-10 mt-16 m-auto'>
+    <section id="about" className="w-4/5 bg-[#F9F8F8] p-10 mt-16 m-auto">
       <div>
-        <div className='text-title	text-3xl font-medium'>
+        <div className="text-title	text-3xl font-medium">
           A propos de nous
         </div>
-        <span className='w-full bg-minibar h-[2px] block my-5'></span>
+        <span className="w-full bg-minibar h-[2px] block my-5"></span>
       </div>
-      <div className='text-xl text-text font-light'>
+      <div className="text-xl text-text font-light">
       Depuis plus de deux décennies, Hold CD s&apos;est imposée comme un acteur de référence dans l&apos;approvisionnement en fournitures industrielles, matières premières et pièces de rechange pour les entreprises industrielles, à l&apos;échelle mondiale. <br></br><br></br>
       Nous avons tissé un vaste réseau de fournisseurs sur les marchés européens et nord-américains, ce qui nous permet d&apos;accéder à des informations précises et fiables en un temps réduit, ainsi qu&apos;à des conditions tarifaires et de livraison avantageuses.
       Nos clients choisissent de déléguer une partie de leur processus d&apos;approvisionnement à Hold CD pour rationaliser leurs opérations et améliorer leur efficacité. Cela se traduit par:
-      <ul className='list-disc ml-10'>
+      <ul className="list-disc ml-10">
         <li>une transmission rapide des informations,</li>
         <li>la disponibilité des produits,</li>
         <li>une rentabilité accrue,</li>
